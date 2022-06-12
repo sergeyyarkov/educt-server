7bc2db44511fc3dbcfb9a9bb3c1c517e75dfb95d
Sergey Yarkov <serzh.yarkov@gmail.com>
2022-06-13 01:02:21 +0300

    removed host env

--- env.ts
+++ env.ts
@@ -15,7 +15,6 @@
 import Env from '@ioc:Adonis/Core/Env';
 
 export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
